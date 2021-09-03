cd4a6191ea30e093c22224f4830b31cfbf4b2ca4
DGUHJH <ghkdwogud852@gmail.com>
2021-09-03 11:22:05 +0900

    Update : styled-components

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+import * as Styled from 'pages/styled';
+
+export default function Home() {
+  return (
+    <Styled.Root>
+      <Head>
+        <title>디소스 (The Source)</title>
+        <meta name="description" content="디소스 - 메인 페이지" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      asd
+    </Styled.Root>
+  );
+}

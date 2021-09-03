7a5b1255dbfdda16b92837780fbb021af18e4923
DGUHJH <ghkdwogud852@gmail.com>
2021-09-03 12:38:08 +0900

    Add : Header Components

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,89 @@
+import headerLogo from 'assets/images/header_logo.png';
+import mainBanner from 'assets/images/main_banner.png';
+import { SearchBar } from 'components/SearchBar';
+import Image from 'next/image';
+import React from 'react';
+import * as Styled from './styled';
+
+const contentMenu = [
+  {
+    label: '인기콘텐츠',
+    url: '',
+  },
+  {
+    label: '이미지',
+    url: '',
+  },
+  {
+    label: '음원소스',
+    url: '',
+  },
+  {
+    label: '영상클립',
+    url: '',
+  },
+  {
+    label: '영상효과',
+    url: '',
+  },
+  {
+    label: '커뮤니티',
+    url: '',
+  },
+];
+
+const userMenu = [
+  {
+    label: '로그인',
+    url: '',
+  },
+  {
+    label: '회원가입',
+    url: '',
+  },
+  {
+    label: '고객센터',
+    url: '',
+  },
+];
+
+export const Header = () => {
+  return (
+    <Styled.Root>
+      <Styled.MenuContainer>
+        <Styled.LeftContainer>
+          <Styled.LogoImageWrapper>
+            <Image src={headerLogo} />
+          </Styled.LogoImageWrapper>
+          {contentMenu.map((value, index) => (
+            <Styled.LeftMenuTypo key={`left_menu_typo_${index}`}>
+              {value.label}
+            </Styled.LeftMenuTypo>
+          ))}
+        </Styled.LeftContainer>
+        <Styled.RightContainer>
+          {userMenu.map((value, index) => (
+            <>
+              {index != 0 && <Styled.RightMenuDivBar />}
+              <Styled.RightMenuTypo key={`right_menu_typo_${index}`}>
+                {value.label}
+              </Styled.RightMenuTypo>
+            </>
+          ))}
+        </Styled.RightContainer>
+      </Styled.MenuContainer>
+      <Styled.BannerContainer>
+        <Styled.BannerWrapper>
+          <Image src={mainBanner} layout="responsive" />
+        </Styled.BannerWrapper>
+        <Styled.SearchBarWrapper>
+          <SearchBar />
+        </Styled.SearchBarWrapper>
+      </Styled.BannerContainer>
+    </Styled.Root>
+  );
+};
+
+export const MHeader = () => {
+  return <div>2</div>;
+};
---
+++ src/components/Header/styled.ts
@@ -0,0 +1,84 @@
+import { Box, Container, Typography } from '@material-ui/core';
+import styled from 'styled-components';
+
+export const Root = styled(Box)`
+  width: 100%;
+  height: 57px;
+  border-bottom: 1px #ccc solid;
+`;
+
+export const MenuContainer = styled(Container)`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0;
+`;
+
+export const LeftContainer = styled(Box)`
+  height: 100%;
+  display: flex;
+  align-items: center;
+`;
+
+export const LogoImageWrapper = styled(Box)`
+  &&& {
+    width: 125px;
+    height: 43px;
+  }
+`;
+
+export const LeftMenuTypo = styled(Typography)`
+  &&& {
+    font-family: NotoSans-Regular;
+    font-size: 16px;
+    color: #575757;
+    margin-left: 45px;
+    cursor: pointer;
+    margin-top: 2px;
+  }
+`;
+
+export const RightContainer = styled(Box)`
+  height: 100%;
+  display: flex;
+  align-items: center;
+`;
+
+export const RightMenuTypo = styled(Typography)`
+  &&& {
+    font-family: NotoSans-Regular;
+    font-size: 14px;
+    color: #5e5e5e;
+    cursor: pointer;
+  }
+`;
+
+export const RightMenuDivBar = styled(Box)`
+  width: 1px;
+  height: 11px;
+  background: #c4c4c4;
+  margin: 0 11px;
+`;
+
+export const BannerContainer = styled(Box)`
+  width: 100%;
+  height: 600px;
+  position: relative;
+  display: flex;
+  justify-content: center;
+`;
+
+export const BannerWrapper = styled(Box)`
+  width: 1920px;
+  height: 600px;
+`;
+
+export const SearchBarWrapper = styled(Box)`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  position: absolute;
+  top: 270px;
+`;

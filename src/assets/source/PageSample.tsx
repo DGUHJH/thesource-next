8eb9b737a00792dbfb8ff1dd366300fed8d25996
DGUHJH <ghkdwogud852@gmail.com>
2021-09-03 12:37:14 +0900

    Add : font - NotoSans

---
+++ src/assets/source/PageSample.tsx
@@ -0,0 +1,31 @@
+import Head from 'next/head';
+import { BrowserView, MobileView } from 'react-device-detect';
+import * as Styled from 'styles/styled';
+
+const Main = () => {
+  return (
+    <Styled.Root>
+      <Head>
+        <title>디소스 (The Source)</title>
+        <meta name="description" content="디소스 - 메인 페이지" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <BrowserView>
+        <Pc />
+      </BrowserView>
+      <MobileView>
+        <Mobile />
+      </MobileView>
+    </Styled.Root>
+  );
+};
+
+const Pc = () => {
+  return <div>1</div>;
+};
+
+const Mobile = () => {
+  return <div>2</div>;
+};
+
+export default Main;

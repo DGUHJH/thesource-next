9ddccae880e98f0dcd89b710a3cc67595d66674c
DGUHJH <ghkdwogud852@gmail.com>
2021-09-06 15:04:15 +0900

    Add : Login Dialog Component

--- src/components/Dialog/Login/index.tsx
+++ src/components/Dialog/Login/index.tsx
@@ -20,11 +20,35 @@ export const LoginDialog: React.FC = () => {
   };
 
   return (
-    <Styled.Root>
-      <Dialog open={loginDialogData.open} onClose={handleClose}>
-        asdad
-      </Dialog>
-    </Styled.Root>
+    <Dialog open={loginDialogData.open} onClose={handleClose}>
+      <Styled.Root>
+        <Styled.TitleTypo>로그인</Styled.TitleTypo>
+        <Styled.Editor
+          placeholder="이메일을 입력해주세요."
+          variant="outlined"
+        />
+        <Styled.Editor
+          placeholder="비밀번호를 입력해주세요."
+          variant="outlined"
+        />
+        <Styled.LoginButton>
+          <Styled.LoginButtonTypo>로그인</Styled.LoginButtonTypo>
+        </Styled.LoginButton>
+        <Styled.FindPasswordTypoWrapper>
+          <Styled.FindPasswordTypo>비밀번호 찾기</Styled.FindPasswordTypo>
+        </Styled.FindPasswordTypoWrapper>
+        <Styled.Bar />
+        <Styled.JoinTypo>
+          디소스의{' '}
+          <Styled.JoinTypoBold> 다양한 콘텐츠들을 </Styled.JoinTypoBold> <br />
+          <Styled.JoinTypoBold> 이용 </Styled.JoinTypoBold>하고 싶다면?
+        </Styled.JoinTypo>
+        <Styled.JoinButton variant="outlined">
+          <Styled.JoinButtonTypo>회원가입 하기</Styled.JoinButtonTypo>
+        </Styled.JoinButton>
+        <Styled.CloseButton onClick={handleClose} />
+      </Styled.Root>
+    </Dialog>
   );
 };
 
--- src/components/Dialog/Login/styled.ts
+++ src/components/Dialog/Login/styled.ts
@@ -1,4 +1,106 @@
-import { Box } from '@material-ui/core';
+import { Box, Button, TextField, Typography } from '@material-ui/core';
+import { Close } from '@material-ui/icons';
 import styled from 'styled-components';
 
-export const Root = styled(Box)``;
+export const Root = styled(Box)`
+  width: 525px;
+  padding: 40px;
+  box-sizing: border-box;
+  position: relative;
+`;
+
+export const TitleTypo = styled(Typography)`
+  &&& {
+    font-family: NotoSans-Medium;
+    font-size: 24px;
+    color: #555;
+  }
+`;
+
+export const Editor = styled(TextField)`
+  &&& {
+    width: 442px;
+    height: 52px;
+    margin-top: 20px;
+  }
+`;
+
+export const LoginButton = styled(Button)`
+  &&& {
+    width: 445px;
+    height: 58px;
+    background: #4ea5fc;
+    margin-top: 20px;
+  }
+`;
+
+export const LoginButtonTypo = styled(Typography)`
+  &&& {
+    font-family: NotoSans-Bold;
+    font-size: 16px;
+    color: #fff;
+  }
+`;
+
+export const FindPasswordTypoWrapper = styled(Box)`
+  width: 100%;
+  display: flex;
+  justify-content: flex-end;
+  margin-top: 20px;
+`;
+
+export const FindPasswordTypo = styled(Typography)`
+  &&& {
+    font-family: NotoSans-Medium;
+    font-size: 13px;
+    color: #aeaeae;
+  }
+`;
+
+export const Bar = styled(Box)`
+  width: 445px;
+  height: 1px;
+  background: #e3e3e3;
+  margin-top: 30px;
+`;
+
+export const JoinTypo = styled(Typography)`
+  &&& {
+    font-family: NotoSans-Medium;
+    font-size: 16px;
+    color: #4b4b4b;
+    margin-top: 15px;
+  }
+`;
+
+export const JoinTypoBold = styled.span`
+  &&& {
+    font-family: NotoSans-Bold;
+  }
+`;
+
+export const JoinButton = styled(Button)`
+  &&& {
+    width: 445px;
+    height: 58px;
+    background: #fff;
+    border-color: #4ea5fc;
+    margin-top: 20px;
+  }
+`;
+
+export const JoinButtonTypo = styled(Typography)`
+  &&& {
+    font-family: NotoSans-Bold;
+    font-size: 16px;
+    color: #4ea5fc;
+  }
+`;
+
+export const CloseButton = styled(Close)`
+  position: absolute;
+  top: 20px;
+  right: 20px;
+  font-size: 4px;
+  cursor: pointer;
+`;
